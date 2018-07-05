2f9d2cc589e9abd54e2d86c4b1ea49a9272fdf46
longweiqiang <573925242@qq.com>
2018-07-05 12:22:44 +0800

    增加容错

--- test.py
+++ test.py
@@ -32,31 +32,14 @@ def GetOss():
     if request.method == "POST":
         path = request.form.get('path')
         times = request.form.get('times')
-        times = int(times)
-        # 换算time,用天数乘以86400秒(一天)
-        time = times * 86400
-        # print(times)
-        auth = oss2.Auth(MyAccessKeyId, MyAccessKeySecret)
-        bucket = oss2.Bucket(auth, MyEndpoint, MyBucketName)
-        oss_url = bucket.sign_url('GET', path, time)
-        code = '00'
-        msg = u'请求成功'
-        dict_data = {
-            "code": code,
-            "msg": msg,
-            "url": oss_url
-        }
-        return Response(json.dumps(dict_data), mimetype='application/json')
-
-    if request.method == "GET":
-        path = request.args.get('path')
-        times = request.args.get('times')
+        # 为path缺失组装一个json
         path_error = {
             "code": 500,
             "msg": "path参数缺失！"
         }
         if path == None:
             return Response(json.dumps(path_error), mimetype='application/json')
+
         elif times == None:
             times = 4
             # 换算time,用天数乘以86400秒(一天)
@@ -73,7 +56,55 @@ def GetOss():
                 "url": oss_url
             }
             return Response(json.dumps(dict_data), mimetype='application/json')
+
         else:
+            # 转换times的数据类型为int
+            times = int(times)
+            # 换算time,用天数乘以86400秒(一天)
+            time = times * 86400
+            # print(times)
+            auth = oss2.Auth(MyAccessKeyId, MyAccessKeySecret)
+            bucket = oss2.Bucket(auth, MyEndpoint, MyBucketName)
+            oss_url = bucket.sign_url('GET', path, time)
+            code = '00'
+            msg = u'请求成功'
+            dict_data = {
+                "code": code,
+                "msg": msg,
+                "url": oss_url
+            }
+            return Response(json.dumps(dict_data), mimetype='application/json')
+
+    if request.method == "GET":
+        path = request.args.get('path')
+        times = request.args.get('times')
+        # 为path缺失组装一个json
+        path_error = {
+            "code": 500,
+            "msg": "path参数缺失！"
+        }
+        if path == None:
+            return Response(json.dumps(path_error), mimetype='application/json')
+
+        elif times == None:
+            times = 4
+            # 换算time,用天数乘以86400秒(一天)
+            time = times * 86400
+            # print(times)
+            auth = oss2.Auth(MyAccessKeyId, MyAccessKeySecret)
+            bucket = oss2.Bucket(auth, MyEndpoint, MyBucketName)
+            oss_url = bucket.sign_url('GET', path, time)
+            code = '00'
+            msg = u'请求成功'
+            dict_data = {
+                "code": code,
+                "msg": msg,
+                "url": oss_url
+            }
+            return Response(json.dumps(dict_data), mimetype='application/json')
+
+        else:
+            # 转换times的数据类型为int
             times = int(times)
             # 换算time,用天数乘以86400秒(一天)
             time = times * 86400
